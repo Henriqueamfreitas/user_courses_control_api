aa23a0f1be272747cd6ec6ec2b4dd94eda5d2e5e
Henrique <you@example.com>
2023-08-16 14:46:49 -0300

    POST

--- src/routers/courses.routers.ts
+++ src/routers/courses.routers.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import "dotenv/config";
-import { validateBodyMiddleware } from "../middlewares/validateBody.middleware";
+import { validateBodyMiddleware, token } from "../middlewares/validateBody.middleware";
 import { courseCreateSchema } from "../schemas/course.schema";
 import { createCourseController } from "../controllers/courses.controllers";
+import { ensureTokenIsAdminMiddleWare } from "../middlewares/verify.middleware";
 
 const coursesRouter: Router = Router()
 
-coursesRouter.post('/', validateBodyMiddleware(courseCreateSchema), createCourseController)
+coursesRouter.use(token)
+coursesRouter.post('/', ensureTokenIsAdminMiddleWare, validateBodyMiddleware(courseCreateSchema), 
+createCourseController)
 
 export { coursesRouter }
